07aa45707b9b56086604a2b25c31caeab9e628eb
BAR-24 <96453433+BAR-24@users.noreply.github.com>
2022-07-25 22:44:02 -0300

    Se termino con jwt, aunque continua arrojando error en la primer consulta de login

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { JwtDto } from 'src/app/models/JwtDto.model';
 import { LoginUsuario } from 'src/app/models/LoginUsuario.model';
 import { Persona } from 'src/app/models/Persona.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { PersonaService } from 'src/app/services/persona.service';
 import { TokenService } from 'src/app/services/token.service';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -14,43 +18,51 @@ import { TokenService } from 'src/app/services/token.service';
 export class HomeComponent implements OnInit {
 
   title = 'miporfolio';
+  isUsrAdmin = false;
   
-  loginUsuario!: LoginUsuario;
-  roles! : string[];
+  loginUsuario: LoginUsuario = new LoginUsuario("user","1234");
   errMjs! : string;
+
 
   public persona: Persona = new Persona(0,"","",0,"","","","",[],[],[],[], undefined);
 
 
-  constructor(private tokenService: TokenService, private authService: AuthService,private personaService : PersonaService) { }
+  constructor(private tokenService  : TokenService, private authService: AuthService, private personaService : PersonaService)
+   { 
 
-  ngOnInit(): void {
 
-    this.loginUsuario = new LoginUsuario("user","1234");
+  }
 
-    this.authService.login(this.loginUsuario).subscribe(data => {
-     
-      this.tokenService.setToken(data.token);
-      this.tokenService.setUsrname(data.usrName);     
-      this.tokenService.setAuthorities(data.authorities);
-      
+  ngOnInit(): void {    
 
-    }, err => {
-     
-      this.errMjs = err.error.message;
-      console.log(this.errMjs);
-      
-    })
+    this.tokenService.getIsUsrAdmin().subscribe( Admin => this.isUsrAdmin = Admin); 
+   
 
-    this.personaService.getPersona().subscribe({
-      next: (response: Persona) =>{
-        this.persona=response;
-        
-      },
-      error:(error:HttpErrorResponse)=>{
-        alert(error.message);
-      }
-    })
+    this.authService.login(this.loginUsuario).subscribe({ 
+      next: (data: JwtDto) =>{
+    
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUsrname(data.usrName);     
+        this.tokenService.setAuthorities(data.authorities);
 
+
+        }, error:(error:HttpErrorResponse)=>{
+          this.errMjs = error.message;
+          console.log(this.errMjs);
+          alert(error.message);
+        }
+     })
+
+    if(this.tokenService.getToken() != null){
+        this.personaService.getPersona().subscribe({
+          next: (response: Persona) =>{
+            this.persona=response;
+            
+          },
+          error:(error:HttpErrorResponse)=>{
+            alert(error.message);
+          }
+        })
+    }
   }
 }
